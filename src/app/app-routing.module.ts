19ef1ed2ca19acc8881c6344313936ae25764748
Graciliano Martins Fontes Neto <graci@graci-mac.local>
2022-03-18 12:37:05 -0300

    Criação da tela de login.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login/login.component';
 import { MenuComponent } from './pages/menu/menu/menu.component';
 
 const routes: Routes = [
   // TODO: Change deafult path to login component
-  { path: '', redirectTo: 'menu', pathMatch: 'full' },
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  {
+    path: 'login',
+    component: LoginComponent,
+    loadChildren: () =>
+      import('./pages/login/login.module').then(
+        (m) => m.LoginModule
+      ),
+  },
   {
     path: 'menu',
     component: MenuComponent,
--- src/app/shared/material.module.ts
+++ src/app/shared/material.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 const materialModules = [
-  MatIconModule
+  MatIconModule,
+  MatCardModule,
+  MatButtonModule,
+  MatFormFieldModule,
+  MatInputModule
 ];
 @NgModule({
   declarations: [],
